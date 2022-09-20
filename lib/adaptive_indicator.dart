55a4f79a6dd344d7925bb68385681a86b298d701
DESKTOP-NH517A7\lenovo <enassaldahbali2060@gmil.com>
2022-09-20 20:12:58 +0300

    first commit

--- lib/adaptive_indicator.dart
+++ lib/adaptive_indicator.dart
@@ -6,7 +6,10 @@ import 'package:flutter/material.dart';
 class AdaptiveIndicator extends StatelessWidget {
 String OS;
 AdaptiveIndicator({required this.OS});
-
+//codee
+  ////////////////\
+  ///this is b
+  ///klmqls,;;;;;;;;;;;;;;
   @override
   Widget build(BuildContext context) {
     if(this.OS=='android')
